8c8c21a3fe4f5453c160704f9e8a1405a428f20b
oreyon <dontrichpeopledifficulut@gmail.com>
2025-04-03 14:12:54 +0800

    refactor: refactor cloudinary.dto from using constructor to use definite assignment

--- src/cloudinary/dto/media.dto.ts
+++ src/cloudinary/dto/media.dto.ts
@@ -1,15 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class RemoveMediaRequest {
-  constructor(secureUrl: string) {
-    this.secureUrl = secureUrl;
-  }
-
   @ApiProperty({
     description: 'Secure URL of the media',
     format: 'url of the media',
     example:
       'https://res.cloudinary.com/example/image/upload/v1234567890/medium',
   })
-  secureUrl: string;
+  secureUrl!: string;
 }
