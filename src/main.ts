354619619428450e99bf10052e9a5d004dc4dd73
oreyon <dontrichpeopledifficulut@gmail.com>
2025-05-12 01:12:52 +0800

    refactor: update import statements for fs and path to use default imports

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { LoggerService } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as fs from 'node:fs';
-import * as path from 'node:path';
+import fs from 'fs';
+import path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
